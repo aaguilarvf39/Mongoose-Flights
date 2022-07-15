ed2e4c5976af34e0065724b8f80ebe3c65716e90
Jesus Aguilar <aaguilarvf39@gmail.com>
2022-07-15 15:45:10 -0700

    adding flights

--- models/flight.js
+++ models/flight.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
-const flightSchema = new Schema({
+const FlightSchema = new Schema({
     airline: String,
     airport: String,
     flightNo: Number,
